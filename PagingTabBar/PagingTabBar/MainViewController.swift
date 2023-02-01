99f89c3ba6988d993032ad783699ae74f023a95d
Evan <jimmy7525@naver.com>
2023-02-01 14:12:03 +0900

    Setup Project

---
+++ PagingTabBar/PagingTabBar/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  PagingTabBar
+//
+//  Created by 윤병일 on 2023/02/01.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+  
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  // MARK: - Functions
+}
